f4fce5866db5c13c90071032b8f7b1820bd614a3
Hyyyyyyyyyy <doudou66035035@outlook.com>
2018-04-01 13:27:05 +0800

    树状数组

--- Ccccccccccc/树状数组.cpp
+++ Ccccccccccc/树状数组.cpp
@@ -1,79 +1,126 @@
 //单点更新，单点查询
-void update(int x, int val)
+int ar[maxn];
+int Bit[maxn];
+int N;
+void Add(int x, int val)
 {
-    int t = x;
-    while(t <= N)
-    {
-        c[t] += val;
-        t += t & -t;
-    }
+	int i;
+	for (i = x; i <= N; i += (i & -i))
+	{
+		Bit[i] += val;
+	}
 }
-int sum(int x)
+int Sum(int x)
 {
-    int t = x;
-    int res = 0;
-    while(t >= 1)
-    {
-        res += c[t];
-        t -= t & -t;
-    }
-    return res;
+	int i;
+	int res = 0;
+	for (i = x; i > 0; i -= (i & -i))
+	{
+		res += Bit[i];
+	}
+	return res;
 }
-update(x);
-sum(x);
+int main()
+{
+    memset(Bit, 0, sizeof(Bit));
+    for (n = 1; n <= N; n++)
+    {
+        scanf("%d", &ar[n]);
+    }
+    //单点查询 查询a
+    scanf("%d", &a);
+    printf("%d\n", ar[a] + Sum(a) - Sum(a-1));
+    //单点修改  把a加c
+    scanf("%d %d", &a, &c);
+    Add(a, c);
+
+}
 
 
 //单点更新，区间查询
-void update(int x, int val)
+int ar[maxn];
+int Bit[maxn];
+int sum[maxn];
+int N;
+void Add(int x, int val)
 {
-    int t = x;
-    while(t <= N)
-    {
-        c[t] += val;
-        t += t & -t;
-    }
+	int i;
+	for (i = x; i <= N; i += (i & -i))
+	{
+		Bit[i] += val;
+	}
 }
-int sum(int x)
+int Sum(int x)
 {
-    int t = x;
-    int res = 0;
-    while(t >= 1)
-    {
-        res += c[t];
-        t -= t & -t;
-    }
-    return res;
+	int i;
+	int res = 0;
+	for (i = x; i > 0; i -= (i & -i))
+	{
+		res += Bit[i];
+	}
+	return res;
 }
-update(x);
-sum(y) - sum(x-1);
+int main()
+{
+    memset(Bit, 0, sizeof(Bit));
+    sum[0] = 0;
+    for (n = 1; n <= N; n++)
+    {
+        scanf("%d", &ar[n]);
+        sum[n] = sum[n - 1] + ar[n];
+    }
+    //区间查询 查询[a,b]
+    scanf("%d %d", &a, &b);
+    printf("%d\n", sum[b] - sum[a-1] + Sum(b) - Sum(a-1));
+    //单点修改 把a加c
+    scanf("%d %d", &a, &c);
+    Add(a, c);
+
+	return 0;
+}
 
 
 
 
 //区间更新， 单点查询
-void update(int x, int val)
+int ar[maxn];
+int Bit[maxn];
+int N;
+void Add(int x, int val)
 {
-    int t = x;
-    while(t <= N)
-    {
-        c[t] += val;
-        t += t & -t;
-    }
+	int i;
+	for (i = x; i <= N; i += (i & -i))
+	{
+		Bit[i] += val;
+	}
 }
-int sum(int x)
+int Sum(int x)
 {
-    int t = x;
-    int res = 0;
-    while(t >= 1)
-    {
-        res += c[t];
-        t -= t & -t;
-    }
-    return res;
+	int i;
+	int res = 0;
+	for (i = x; i > 0; i -= (i & -i))
+	{
+		res += Bit[i];
+	}
+	return res;
 }
-update(x, v);
-update(y+1, -v);
-sum(x);
+int main()
+{
+    memset(Bit, 0, sizeof(Bit));
+    for (n = 1; n <= N; n++)
+    {
+        scanf("%d", &ar[n]);
+    }
+    //单点查询 查询a
+    scanf("%d", &a);
+    printf("%d\n", ar[a] + Sum(a));
+    //区间更新(差分法) 更新[a,b] + c
+    scanf("%d %d %d", &a, &b, &c);
+    Add(a, c);
+    Add(b + 1, -c);
+	return 0;
+}
+
 
 
 
